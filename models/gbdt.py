a9a1631513182efe591e1cbd6370a6f82ba5effc
Alex Klibisz <aklibisz@gmail.com>
2017-12-13 13:06:43 -0500

    Started adding interaction features to GBDT

--- models/gbdt.py
+++ models/gbdt.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from hashlib import md5
+from itertools import product
 from more_itertools import flatten
 from pprint import pformat
 from scipy.sparse import csr_matrix, save_npz, load_npz
@@ -7,7 +8,6 @@ from time import time
 from tqdm import tqdm
 import argparse
 import json
-import lightgbm as lgbm
 import logging
 import numpy as np
 import os
@@ -15,10 +15,13 @@ import pandas as pd
 import pdb
 import re
 
+import lightgbm as lgbm
+
+# from keras.optimizers import Adagrad
+
 GBDT_PARAMS_DEFAULT = {
 
     # Defining the task.
-    'application': 'binary',
     'objective': 'binary',
     'boosting': 'gbdt',
     'metric': 'auc',
@@ -26,37 +29,45 @@ GBDT_PARAMS_DEFAULT = {
 
     # How many learners to fit, and how long to continue without
     # improvement on the validation set.
-    'num_iterations': 475,
+    'num_iterations': 1000,
     'early_stopping_rounds': 50,
 
     # TODO: explain these parameters.
-    'learning_rate': 0.4,
+    'learning_rate': 0.3,
     'max_bin': 255,
 
     # Constraints on the tree characteristics.
     # Generally larger values will fit better but may over-fit.
     'max_depth': 10,
-    'num_leaves': 64,
+    'num_leaves': 108,
 
     # Randomly select *bagging_fraction* of the data to fit a learner.
     # Perform bagging at every *bagging_freq* iterations.
     # Seed the random bagging with *bagging_seed*.
-    'bagging_fraction': 1.0,
-    'bagging_freq': 0,
+    'bagging_fraction': 0.95,
+    'bagging_freq': 1,
     'bagging_seed': 1,
 
     # Randomly select *feature_fraction* of the features to fit a learner.
     # Seed the random selection with *feature_fraction_seed*.
-    'feature_fraction': 1.0,
+    'feature_fraction': 0.9,
     'feature_fraction_seed': 1,
 
 }
 
-# Regular expressions and function for parsing musicians.
-RE_MUSICIANS_SPLIT_PATTERN = re.compile(r'feat(.)\w*|\(|\)|\||\/')
+INTERACTION_SPACE_PARAMS_DEFAULT = {
+    'vec_size': 50,
+    'vecs_init_func': np.random.normal,
+    'vecs_init_kwargs': {'loc': 0, 'scale': 0.05},
+    # 'optimizer': Adagrad,
+    'optimizer_kwargs': {'lr': 0.01},
+    'nb_epochs_max': 10,
+    'batch_size': 50000
+}
 
 
 def save_best_model(check_every_iterations, name, metric, mode, model_path, params_path=None):
+    """LGBM callback."""
     metric_vals = []
     improved_iterations = [-1]
     cmpfunc = np.argmax if mode == 'max' else np.argmin
@@ -92,7 +103,7 @@ def save_best_model(check_every_iterations, name, metric, mode, model_path, para
 
 
 def print_feature_importance(print_every_iterations=10, importance_type='gain'):
-
+    """LGBM callback."""
     def callback(env):
         if env.iteration % print_every_iterations != 0:
             return
@@ -105,6 +116,9 @@ def print_feature_importance(print_every_iterations=10, importance_type='gain'):
 
     callback.order = 99
     return callback
+
+# Regular expressions and function for parsing musicians.
+RE_MUSICIANS_SPLIT_PATTERN = re.compile(r'feat(.)\w*|\(|\)|\||\/')
 
 
 def parse_musicians(mstr):
@@ -133,19 +147,25 @@ def parse_isrc_year(isrc):
     return int('%d%02d' % (y0, y1))
 
 
+def round_to_nearest(n, r):
+    return round(n / r) * r
+
+
 class GBDTRec(object):
 
-    def __init__(self, artifacts_dir, data_dir='data'):
+    def __init__(self, artifacts_dir, data_dir='data',
+                 interaction_space_params=INTERACTION_SPACE_PARAMS_DEFAULT):
         self.artifacts_dir = artifacts_dir
+        self.interaction_space_params = interaction_space_params
         self.data_dir = data_dir
         self.logger = logging.getLogger(self.__class__.__name__)
 
-    def _feature_transformations(self, CMB):
+    def _get_base_features(self, feats_cmb):
 
         # Rename user and song ids for consistency.
         # Others will be renamed later.
-        CMB.rename({'msno': 'user_id_cat', 'song_id': 'song_id_cat'},
-                   axis='columns', inplace=True)
+        feats_cmb.rename({'msno': 'user_id_cat', 'song_id': 'song_id_cat'},
+                         axis='columns', inplace=True)
 
         #########
         # USERS #
@@ -153,11 +173,13 @@ class GBDTRec(object):
 
         # Get unique users and transform them.
         user_cols = ['user_id_cat', 'bd', 'city', 'gender', 'registration_init_time']
-        U = CMB[user_cols].drop_duplicates('user_id_cat')
+        U = feats_cmb[user_cols].drop_duplicates('user_id_cat')
         self.logger.info('Transforming %d unique users' % len(U))
 
-        # Clip the ages at a reasonable value and rename.
-        U['bd'].clip(5, 70, inplace=True)
+        # Clip the ages at a reasonable value and rename. Replace unreasonable
+        # values with NaNs.
+        fix_age = lambda x: min(max(x, 10), 60) if 10 <= x <= 60 else np.nan
+        U['bd'] = U['bd'].apply(fix_age)
         U.rename({'bd': 'user_age_con'}, axis='columns', inplace=True)
 
         # Keep city as-is and rename.
@@ -182,7 +204,7 @@ class GBDTRec(object):
         song_cols = ['song_id_cat', 'artist_name', 'composer', 'genre_ids', 'isrc',
                      'language', 'lyricist', 'song_length']
         song_hash_cols = list(set(song_cols) - {'song_id_cat'})
-        S = CMB[song_cols].drop_duplicates('song_id_cat')
+        S = feats_cmb[song_cols].drop_duplicates('song_id_cat')
         self.logger.info('Transforming %d unique songs' % len(S))
 
         # Replace unknown artists, lyricists, composers. '佚名' means missing names.
@@ -197,6 +219,9 @@ class GBDTRec(object):
         S['language'].replace(24, 3, inplace=True)
         S['language'].replace(59, 3, inplace=True)
 
+        # Replace negative language value with positive.
+        S['language'].replace(-1, 1, inplace=True)
+
         # Compute song hashes. Later these will replace the song IDs.
         self.logger.info('Hashing songs')
         get_hash = lambda row: md5(str(row.values.tolist()).encode()).hexdigest()
@@ -204,6 +229,7 @@ class GBDTRec(object):
 
         # Leave song length as is and rename.
         S.rename({'song_length': 'song_len_con'}, axis='columns', inplace=True)
+        pdb.set_trace()
 
         # Extract song country and year from ISRC.
         get_country = lambda x: x[:2] if type(x) is str else np.nan
@@ -217,7 +243,7 @@ class GBDTRec(object):
         self.logger.info('Parsing, counting musicians and genres')
         song_id_2_musicians = {}
         song_id_2_genres = {}
-        song_id_counter = Counter(CMB['song_id_cat'].values)
+        song_id_counter = Counter(feats_cmb['song_id_cat'].values)
         musicians_counter = Counter()
         genres_counter = Counter()
         for i, row in S.iterrows():
@@ -255,84 +281,135 @@ class GBDTRec(object):
 
         # Keep only the new columns.
         S = S[['song_id_cat', 'song_hash_cat', 'song_len_con', 'song_country_cat',
-               'song_year_con', 'song_musician_cat', 'song_genre_cat']]
+               'song_year_con', 'song_musician_cat', 'song_genre_cat', 'song_language_cat']]
 
         ###########
         # CONTEXT #
         ###########
 
         # Clean up context variables.
-        CMB['source_screen_name'].replace('Unknown', np.nan, inplace=True)
-        CMB['source_system_tab'].replace('null', np.nan, inplace=True)
-        CMB.rename({'source_screen_name': 'ctxt_scr_cat',
-                    'source_system_tab': 'ctxt_tab_cat',
-                    'source_type': 'ctxt_type_cat'},
-                   axis='columns', inplace=True)
+        feats_cmb['source_screen_name'].replace('Unknown', np.nan, inplace=True)
+        feats_cmb['source_system_tab'].replace('null', np.nan, inplace=True)
+        feats_cmb.rename({'source_screen_name': 'ctxt_scr_cat',
+                          'source_system_tab': 'ctxt_tab_cat',
+                          'source_type': 'ctxt_type_cat'},
+                         axis='columns', inplace=True)
 
         # Keep subset of columns.
-        CMB = CMB[['user_id_cat', 'song_id_cat', 'ctxt_scr_cat', 'ctxt_tab_cat',
-                   'ctxt_type_cat', 'target']]
+        feats_cmb = feats_cmb[['user_id_cat', 'song_id_cat', 'ctxt_scr_cat', 'ctxt_tab_cat',
+                               'ctxt_type_cat', 'target']]
 
         ###########
         # MERGING #
         ###########
 
-        # Left join CMB with the users and songs.
-        CMB = CMB.merge(U, on='user_id_cat', how='left')
-        CMB = CMB.merge(S, on='song_id_cat', how='left')
+        # Left join feats_cmb with the users and songs.
+        feats_cmb = feats_cmb.merge(U, on='user_id_cat', how='left')
+        feats_cmb = feats_cmb.merge(S, on='song_id_cat', how='left')
 
         # Replace the song id with song hash.
-        CMB['song_id_cat'] = CMB['song_hash_cat']
-        CMB.drop('song_hash_cat', inplace=True, axis=1)
-        return CMB
+        feats_cmb['song_id_cat'] = feats_cmb['song_hash_cat']
+        feats_cmb.drop('song_hash_cat', inplace=True, axis=1)
+        return feats_cmb
+
+    def _get_interaction_features(self, feats_base):
+
+        self.logger.info('Discretizing continuous features')
+        feats_intr = feats_base.copy()
+        feats_intr = feats_intr[['user_id_cat', 'song_id_cat', 'song_musician_cat', 'song_genre_cat']]
+
+        # round_age = lambda x: x if np.isnan(x) else round_to_nearest(x, 3)
+        # feats_intr['user_age_cat'] = feats_intr['user_age_con'].apply(round_age).astype('category')
+        # feats_intr.drop('user_age_con', axis=1, inplace=True)
+
+        # round_year = lambda x: x if np.isnan(x) else round_to_nearest(x, 3)
+        # feats_intr['user_regyear_cat'] = feats_intr['user_regyear_con'].apply(round_year).astype('category')
+        # feats_intr.drop('user_regyear_con', axis=1, inplace=True)
+
+        # feats_intr['song_len_cat'] = np.log(feats_intr['song_len_con']).round().astype('category')
+        # feats_intr.drop('song_len_con', axis=1, inplace=True)
+
+        # Encode all possible features into a term<->index vocabulary.
+        # nan (missing) is encoded as index 0.
+        feat_terms_all = set()
+        for c in feats_intr.columns:
+            set_prefix = lambda v: '%s::%s' % (c, str(v))
+            feat_terms_new = map(set_prefix, feats_intr[c].unique().tolist())
+            feat_terms_all = feat_terms_all.union(feat_terms_new)
+
+        self.logger.info('Found %d unique feature terms' % len(feat_terms_all))
+
+        pdb.set_trace()
+
+        # Get the interaction product of user and song columns.
+        cols_user = [c for c in feats_intr.columns if c.startswith('user_')]
+        cols_song = [c for c in feats_intr.columns if c.startswith('song_')]
+        cols_prod = list(product(cols_user, cols_song))
+
+        pdb.set_trace()
+
+        # Expand the rows into index pairs with a target.
+
+        # Initialize and fit the vector space model.
+
+        # Store the pairwise similarities as features.
+
+        pdb.set_trace()
 
     def get_features(self, which='train'):
 
         assert which in {'train', 'test'}
-
-        path_trn = '%s/data-trn.csv' % self.artifacts_dir
-        path_tst = '%s/data-tst.csv' % self.artifacts_dir
-        feats_ready = os.path.exists(path_trn) and os.path.exists(path_tst)
         TRN = TST = None
 
-        if not feats_ready:
+        # FIXME: should be just feats-base.csv..
+        path_feats_base = '%s/feats-base-trn.csv' % self.artifacts_dir
+        path_feats_intr = '%s/feats-interactions.csv' % self.artifacts_dir
+        path_model_intr = '%s/model-interaction-space.hdf5' % self.artifacts_dir
+
+        ready_feats_base = os.path.exists(path_feats_base)
+        ready_feats_intr = os.path.exists(path_feats_intr) and os.path.exists(path_model_intr)
+
+        if not ready_feats_base:
             t0 = time()
             self.logger.info('Reading dataframes')
-            SNG = pd.read_csv('%s/songs.csv' % self.data_dir)
-            SEI = pd.read_csv('%s/song_extra_info.csv' % self.data_dir)
-            MMB = pd.read_csv('%s/members.csv' % self.data_dir)
-            TRN = pd.read_csv('%s/train.csv' % self.data_dir)
-            TST = pd.read_csv('%s/test.csv' % self.data_dir)
+            feats_sng = pd.read_csv('%s/songs.csv' % self.data_dir)
+            feats_sei = pd.read_csv('%s/song_extra_info.csv' % self.data_dir)
+            feats_mmb = pd.read_csv('%s/members.csv' % self.data_dir)
+            feats_trn = pd.read_csv('%s/train.csv' % self.data_dir)
+            feats_tst = pd.read_csv('%s/test.csv' % self.data_dir)
 
             self.logger.info('Merging dataframes')
-            TRN = TRN.merge(MMB, on='msno', how='left')
-            TST = TST.merge(MMB, on='msno', how='left')
-            TRN = TRN.merge(SNG, on='song_id', how='left')
-            TST = TST.merge(SNG, on='song_id', how='left')
-            TRN = TRN.merge(SEI, on='song_id', how='left')
-            TST = TST.merge(SEI, on='song_id', how='left')
+            feats_trn = feats_trn.merge(feats_mmb, on='msno', how='left')
+            feats_tst = feats_tst.merge(feats_mmb, on='msno', how='left')
+            feats_trn = feats_trn.merge(feats_sng, on='song_id', how='left')
+            feats_tst = feats_tst.merge(feats_sng, on='song_id', how='left')
+            feats_trn = feats_trn.merge(feats_sei, on='song_id', how='left')
+            feats_tst = feats_tst.merge(feats_sei, on='song_id', how='left')
 
-            self.logger.info('Combining TRN and TST')
-            CMB = TRN.append(TST, ignore_index=True)
-
-            nb_trn = len(TRN)
-            nb_tst = len(TST)
+            self.logger.info('Combining feats_trn and feats_tst')
+            feats_cmb = feats_trn.append(feats_tst, ignore_index=True)
 
             # Save some memory.
-            del SNG, SEI, MMB, TRN, TST
+            del feats_sng, feats_sei, feats_mmb, feats_trn, feats_tst
 
             # Encode test and train rows at the same time.
-            self.logger.info('Transforming features')
-            CMB = self._feature_transformations(CMB)
-            TRN = CMB.iloc[:nb_trn]
-            TST = CMB.iloc[-nb_tst:]
-            TRN.to_csv(path_trn, index=False)
-            TST.to_csv(path_tst, index=False)
+            self.logger.info('Engineering base features')
+            feats_base = self._get_base_features(feats_cmb)
+            feats_base.to_csv(path_feats_base, index=False)
             self.logger.info('Completed in %d seconds' % (time() - t0))
+
+        if not ready_feats_intr:
+            self.logger.info('Engineering interaction features')
+            feats_base = pd.read_csv(path_feats_base, nrows=500000)
+            feats_intr = self._get_interaction_features(feats_base)
+
+            # ispace = InteractionSpace(**self.interaction_space_params)
+
+        # TODO: compute interaction features and add them to the base features.
 
         if which == 'train':
             if TRN is None:
-                TRN = pd.read_csv(path_trn)
+                TRN = pd.read_csv(path_base_trn, nrows=100000)
             for c in TRN.columns:
                 if c.endswith('_cat'):
                     TRN[c] = TRN[c].astype('category')
@@ -340,7 +417,7 @@ class GBDTRec(object):
 
         elif which == 'test':
             if TST is None:
-                TST = pd.read_csv(path_tst)
+                TST = pd.read_csv(path_base_tst)
             for c in TST.columns:
                 if c.endswith('_cat'):
                     TST[c] = TST[c].astype('category')
@@ -415,6 +492,20 @@ class GBDTRec(object):
         df.to_csv(submission_path, index=False)
         self.logger.info('Saved predictions %s' % submission_path)
 
+
+class InteractionSpace(object):
+
+    def __init__(self, df1, df2, model_path):
+
+        return
+
+    def fit(self):
+
+        return
+
+    def eval(self):
+
+        return
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
